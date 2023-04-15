33a526d3dd717507ce35b0314314bd72d3017f57
Lizette Pérez <lizette.perezcorrea@gmail.com>
2023-04-15 00:28:31 -0400

    update tabla

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,61 +1,40 @@
-const URL_BASE = 'https://rickandmortyapi.com/api';
-const URL_CHARACTERS = URL_BASE + '/character';
+const URL_BASE = 'https://digimon-api.vercel.app/api';
+const URL_DIGIMON = URL_BASE + '/digimon';
 
 let contenido;
-let carta;
-
-function tarjeta(data) {
-  carta.innerHTML = "";
-
-  for (let temp of data) {
-    carta.innerHTML +=
-      `
-      <div class="card" style="width: 18rem;">
-        <img src="${temp.image}" alt="${temp.name}" title="${temp.name}" class="card-img-top">
-        <div class="card-body">
-          <h5 class="card-title">${temp.name}</h5>
-          <p class="card-text">ESPECIE: ${temp.species}</p>
-        </div>
-      </div>
-    `;
-  }
-}
-
-function capturarDato() {
-  let nombrePersonaje = document.getElementById("dato").value;
-  nombrePersonaje = nombrePersonaje.toLowerCase();
-  document.getElementById("tabla-principal").style.display = "none";
-  document.getElementById("carta").style.display = "block";
-
-  fetch(URL_CHARACTERS + '/?name=' + nombrePersonaje)
-    .then(response => response.json())
-    .then(datos => {
-      console.log(datos);
-      tarjeta(datos.results);
-    });
-}
 
 function tabla(datos) {
   contenido.innerHTML = "";
 
-  for (let temp of datos) {
-    contenido.innerHTML += `<tr>
-      <th scope="row">${temp.name}</th>
-      <td><img src="${temp.image}" alt="${temp.name}" title="${temp.name}" width="100" height="100"></td>
-      <td>${temp.species}</td>
-    </tr>`;
+  for (let digimon of datos) {
+    const row = document.createElement("tr");
+
+    row.innerHTML += `
+          <th scope="row">${digimon.name}</ th> 
+          <td><img width="100px" height="100px" src="${digimon.img}" alt="${digimon.name}"></td> 
+          <td>${digimon.level}</td>
+      </tr> `;
+    row.addEventListener("click", function () {
+      goToDigimonPerfil(digimon.name);
+    });
+    contenido.appendChild(row);
   }
 }
 
+function goToDigimonPerfil(digimonName) {
+  let digimonPerfilURL = "./digimon-perfil.html?name=" + digimonName;
+  window.location.href = digimonPerfilURL;
+}
+
+
 $(document).ready(function () {
   contenido = document.getElementById("contenido");
-  carta = document.getElementById("carta");
 
-  fetch(URL_CHARACTERS)
+  fetch(URL_DIGIMON)
     .then(response => response.json())
     .then(datos => {
       console.log(datos);
-      tabla(datos.results);
+      tabla(datos);
     });
 
 });
