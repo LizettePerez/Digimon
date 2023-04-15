57d9a3f466f2f07a6e9f1253f107338cfc70d491
Lizette Pérez <lizette.perezcorrea@gmail.com>
2023-04-14 22:35:00 -0400

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,9 +2,37 @@ const URL_BASE = 'https://rickandmortyapi.com/api';
 const URL_CHARACTERS = URL_BASE + '/character';
 
 let contenido;
+let carta;
+
+function tarjeta(data) {
+  carta.innerHTML = "";
+
+  for (let temp of data) {
+    carta.innerHTML +=
+      `
+      <div class="card" style="width: 18rem;">
+        <img src="${temp.image}" alt="${temp.name}" title="${temp.name}" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">${temp.name}</h5>
+          <p class="card-text">ESPECIE: ${temp.species}</p>
+        </div>
+      </div>
+    `;
+  }
+}
 
 function capturarDato() {
+  let nombrePersonaje = document.getElementById("dato").value;
+  nombrePersonaje = nombrePersonaje.toLowerCase();
+  document.getElementById("tabla-principal").style.display = "none";
+  document.getElementById("carta").style.display = "block";
 
+  fetch(URL_CHARACTERS + '/?name=' + nombrePersonaje)
+    .then(response => response.json())
+    .then(datos => {
+      console.log(datos);
+      tarjeta(datos.results);
+    });
 }
 
 function tabla(datos) {
@@ -21,10 +49,12 @@ function tabla(datos) {
 
 $(document).ready(function () {
   contenido = document.getElementById("contenido");
+  carta = document.getElementById("carta");
 
   fetch(URL_CHARACTERS)
     .then(response => response.json())
     .then(datos => {
+      console.log(datos);
       tabla(datos.results);
     });
 
