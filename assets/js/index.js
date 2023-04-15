82915b7c09e20b79eba173145f34a359e800bc5a
Lizette Pérez <lizette.perezcorrea@gmail.com>
2023-04-14 21:45:26 -0400

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,31 @@
 const URL_BASE = 'https://rickandmortyapi.com/api';
+const URL_CHARACTERS = URL_BASE + '/character';
 
-fetch(URL_BASE + '/character');
+let contenido;
+
+function capturarDato() {
+
+}
+
+function tabla(datos) {
+  contenido.innerHTML = "";
+
+  for (let temp of datos) {
+    contenido.innerHTML += `<tr>
+      <th scope="row">${temp.name}</th>
+      <td><img src="${temp.image}" alt="${temp.name}" title="${temp.name}" width="100" height="100"></td>
+      <td>${temp.species}</td>
+    </tr>`;
+  }
+}
+
+$(document).ready(function () {
+  contenido = document.getElementById("contenido");
+
+  fetch(URL_CHARACTERS)
+    .then(response => response.json())
+    .then(datos => {
+      tabla(datos.results);
+    });
+
+});
