da1f2862654456e8892591de49bf99c5abc95a06
Lizette Pérez <lizette.perezcorrea@gmail.com>
2023-04-16 00:13:26 -0400

    update css

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,105 +1,138 @@
 const URL_BASE = 'https://digimon-api.vercel.app/api';
 const URL_DIGIMON = URL_BASE + '/digimon';
 
-let contenido;
+let digimonContainer;
 
 function tabla(datos) {
-  contenido.innerHTML = "";
+  digimonContainer.innerHTML = "";
 
   for (let digimon of datos) {
-
-    contenido.innerHTML += `
-          <th scope="row">${digimon.name}</ th> 
-          <td><img width="100px" height="100px" src="${digimon.img}" alt="${digimon.name}"></td> 
-          <td>${digimon.level}</td>
-      </tr> `;
+    digimonContainer.innerHTML += `
+      <div class="card border-primary" style="width: 14rem;">
+        <img src="${digimon.img}" class="card-img-top" alt="${digimon.name}" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal${digimon.name}">
+        <div class="card-body">
+          <h5 class="card-title text-dark">${digimon.name}</h5>
+          <p class="card-text text-secondary">${digimon.level}</p>
+        </div>
+      </div>
+      <div class="modal fade" id="exampleModal${digimon.name}" tabindex="-1" aria-labelledby="exampleModalLabel${digimon.name}" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel${digimon.name}">${digimon.name}</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <img src="${digimon.img}" alt="${digimon.name}" title="${digimon.name}" >
+            <div class="modal-body">
+            <h6>LEVEL</h6>
+              ${digimon.level}
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>`;
   }
 }
 
-function tarjeta(data) {
-  carta.innerHTML = "";
-
-  for (let digimon of data) {
-    carta.innerHTML += ` 
-        <div id="tarjSola" class="card mb-3 container" style="max-width: 540px;">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src="${digimon.img}" class="img-fluid rounded-start" alt="...">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">${digimon.name}</h5>
-        <p class="card-text">${digimon.level}</p>
-       </div>
-    </div>
-  </div>
-</div>
-   `
-  }
-}
 
 
 
 function searchName() {
   let nombreDigimon = document.getElementById("inputName").value.toLowerCase();
-  document.getElementById("tabla_principal").style.display = "none";
-
-
 
   if (nombreDigimon === "") {
     fetch(URL_DIGIMON)
       .then(response => response.json())
       .then(datos => {
-        tabla_principal.style.display = "table";
         tabla(datos);
-        carta.style.display = "none";
       });
   } else {
     fetch(URL_DIGIMON + '/name/' + nombreDigimon)
       .then(response => response.json())
       .then(datos => {
-        tarjeta(datos);
-      });
-  }
-}
-
-function searchLevel() {
-  let levelDigimon = document.getElementById("inputLevel").value.toLowerCase();
-
-  if (levelDigimon === "") {
-    fetch(URL_DIGIMON)
-      .then(response => response.json())
-      .then(datos => {
-        tabla(datos);
-      });
-  } else {
-    fetch(URL_DIGIMON + '/level/' + levelDigimon)
-      .then(response => response.json())
-      .then(datos => {
         tabla(datos);
       });
   }
 }
 
-function sortByName(order) {
+
+
+function sortByLevel(level) {
+  fetch(URL_DIGIMON + '/level/' + level)
+    .then(response => response.json())
+    .then(datos => {
+      tabla(datos);
+    });
+}
+
+function sortByAllLevel(orden) {
+
   fetch(URL_DIGIMON)
     .then(response => response.json())
     .then(datos => {
-      if (order === "asc") {
-        datos.sort((a, b) => a.name.localeCompare(b.name));
-      } else if (order === "desc") {
-        datos.sort((a, b) => b.name.localeCompare(a.name));
+      if (orden === 'desc') {
+        datos.sort((a, b) => {
+          if (a.level > b.level) {
+            return -1;
+          } else if (a.level < b.level) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+      } else {
+        datos.sort((a, b) => {
+          if (a.level < b.level) {
+            return -1;
+          } else if (a.level > b.level) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
       }
       tabla(datos);
     });
 }
 
 
+function sortByName(orden) {
+
+  fetch(URL_DIGIMON)
+    .then(response => response.json())
+    .then(datos => {
+      if (orden === 'desc') {
+        datos.sort((a, b) => {
+          if (a.name > b.name) {
+            return -1;
+          } else if (a.name < b.name) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+      } else {
+        datos.sort((a, b) => {
+          if (a.name < b.name) {
+            return -1;
+          } else if (a.name > b.name) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+      }
+
+      tabla(datos);
+    });
+}
+
 
 
 $(document).ready(function () {
-  contenido = document.getElementById("contenido");
-  carta = document.getElementById("carta");
+  digimonContainer = document.getElementById("digimonContainer");
 
   fetch(URL_DIGIMON)
     .then(response => response.json())
